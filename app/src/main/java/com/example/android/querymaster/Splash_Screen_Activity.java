2902703dde7666e8fd679217dbf693799189f892
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2021-11-28 21:46:32 +0530

    splash screen added

---
+++ app/src/main/java/com/example/android/querymaster/Splash_Screen_Activity.java
@@ -0,0 +1,25 @@
+package com.example.android.querymaster;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+
+public class Splash_Screen_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        Runnable runnable = () -> {
+            Intent intent = new Intent(Splash_Screen_Activity.this, Login_Activity.class);
+            startActivity(intent);
+        };
+        handler.postDelayed(runnable, 500);
+    }
+}
